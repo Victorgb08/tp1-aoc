11599668dd294ff2767cb823d6703eafe0f72ffb
Victor <victor.guedes.08@gmail.com>
2022-10-31 11:06:49 -0300

    .

--- tp1.c
+++ tp1.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
  
-/* returns count of numbers which are in range from 1 to n and don't contain 3
-   as a digit */
 int count(int n)
 {
-    // Base cases (Assuming n is not negative)
+    // Casos básicos considerando n>0
     if (n < 3)
         return n;
     if (n >= 3 && n < 10)
        return n-1;
  
-    // Calculate 10^(d-1) (10 raise to the power d-1) where d is
-    // number of digits in n. po will be 100 for n = 578
+    // Cálculo da potência de 10 do número
     int po = 1;
     while (n/po > 9)
         po = po*10;
  
-    // find the most significant digit (msd is 5 for 578)
-    int msd = n/po;
+    // Cálculo do algarismo mais significativo
+    int ams = n/po;
  
-    if (msd != 3)
-      // For 578, total will be 4*count(10^2 - 1) + 4 + count(78)
-      return count(msd)*count(po - 1) + count(msd) + count(n%po);
+    if (ams != 3)
+      // Para 636 (ams=6, po=100) total vai ser 5*count(100 - 1) + 4 + count(36)
+      return count(ams)*count(po - 1) + count(ams) + count(n%po);
+      // Cálculo dos números que não tem 3 na potência do número (5*80) + os inteiros que
+      // são desconsiderados do cálculo anterior(5) + resto do número(26)
+  
     else
-      // For 35, total will be equal to count(29)
-      return count(msd*po - 1);
+      // Pega o maior número na potência - 1 (Ex: para 33 -> count(30-1))
+      return count(ams*po - 1);
 }
  
-// Driver program to test above function
 int main()
 {
-    printf ("%d ", count(578));
+    int a;
+    scanf("%i", &a);
+    printf ("%d\n", count(a));
     return 0;
 }
